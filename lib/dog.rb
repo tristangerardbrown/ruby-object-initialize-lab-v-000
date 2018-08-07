4683f3a86a074a39561035bc3aad2305cc516780
Tristan Brown <tgbrown@stanford.edu>
2018-08-07 00:14:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,17 +4,13 @@ class Dog
     @breed=breed
   end
 
-  def name=(name)
+  def dog=(name, breed)
     @name=name
-  end
-  def name
-    @name
-  end
-
-  def breed=(breed)
     @breed=breed
   end
-  def breed
+
+  def dog
+    @name
     @breed
   end
-end 
+end
